0749e28d70c332b6a87a05167d2efe388c045efc
Mo Bitar <me@mbbitar.com>
2014-08-21 17:12:26 -0500

    updates

--- Instagram/InstagramAPIHelper.m
+++ Instagram/InstagramAPIHelper.m
@@ -72,13 +72,13 @@
 
 - (void)followUserId:(NSString*)userId withAccessToken:(NSString*)accessToken
 {
-    NSString *path = [NSString stringWithFormat:@"https://api.instagram.com/v1/users/%@/relationship?access_token=%@", userId, accessToken];
-    NSDictionary *parameters = @{@"action" : @"follow"};
-    [[AppDelegate serverClient] postPath:path parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
-//        NSLog(operation.responseString);
-    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-//        NSLog(error.description);
-    }];
+//    NSString *path = [NSString stringWithFormat:@"https://api.instagram.com/v1/users/%@/relationship?access_token=%@", userId, accessToken];
+//    NSDictionary *parameters = @{@"action" : @"follow"};
+//    [[AppDelegate serverClient] postPath:path parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
+////        NSLog(operation.responseString);
+//    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+////        NSLog(error.description);
+//    }];
 }
 
 @end
